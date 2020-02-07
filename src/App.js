e1a25ddce757b8064798892180be5c0e60fd7671
TommiS <tommi.supi@gmail.com>
2020-02-07 15:16:51 +0200

    tehty lomakkeet

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import logo from './500px-Steering_wheel_ship.png';
 import './App.css';
+import Lomake from "./Lomake";
+import Lomaketiedot from "./Lomaketiedot";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+    state = { id: "", title:"", description:"", additionalSource:"", complete:"", creationDate:"", completionDate:""};
+    yhdistaja = (topic) => {
+        this.setState({id: topic.id, title: topic.title, description: topic.description, additionalSource: topic.additionalSource, complete: topic.complete, creationDate: topic.creationDate, completionDate: topic.completionDate})
+    };
+    render() {
+        return (
+            <div className="App">
+
+                <h1>Oppimispäiväkirja</h1>
+                <Lomake viittaus={this.yhdistaja}/>
+                <Lomaketiedot tiedot={this.state}/>
+                <img src={logo} className="App-logo" alt="logo" />
+
+            </div>
+        );
+    }}
 
 export default App;
---
+++ src/Lomaketiedot.jsx
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+class Lomaketiedot extends Component {
+    render() {
+        // this.props.tiedot {nimi, epost}
+        return (
+            <div>
+                <h3>Lomaketiedot</h3>
+                <p>id: {this.props.tiedot.id}</p>
+                <p>title: {this.props.tiedot.title}</p>
+                <p>description: {this.props.tiedot.description}</p>
+                <p>additionalSource: {this.props.tiedot.additionalSource}</p>
+                <p>complete: {this.props.tiedot.complete}</p>
+                <p>creationDate: {this.props.tiedot.creationDate}</p>
+                <p>completionDate: {this.props.tiedot.completionDate}</p>
+            </div>
+        );
+    }
+}
+
+export default Lomaketiedot;
