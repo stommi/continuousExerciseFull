8ddde84bf9738ba74dca3c2dd700b9cdc2d5a267
TommiS <tommi.supi@gmail.com>
2020-02-14 14:54:31 +0200

    fixed

--- src/TopicList.jsx
+++ src/TopicList.jsx
@@ -5,13 +5,13 @@ class TopicList extends Component {
     render() {
         return (<table>
             <tbody>
-            <Topic tiedot={this.props.quote[0]}/>
-            <Topic tiedot={this.props.quote[1]}/>
-            <Topic tiedot={this.props.quote[2]}/>
-            <Topic tiedot={this.props.quote[3]}/>
-            <Topic tiedot={this.props.quote[4]}/>
-            <Topic tiedot={this.props.quote[5]}/>
-            <Topic tiedot={this.props.quote[6]}/>
+            <Topic tiedot={this.props.topic[0]}/>
+            <Topic tiedot={this.props.topic[1]}/>
+            <Topic tiedot={this.props.topic[2]}/>
+            <Topic tiedot={this.props.topic[3]}/>
+            <Topic tiedot={this.props.topic[4]}/>
+            <Topic tiedot={this.props.topic[5]}/>
+            <Topic tiedot={this.props.topic[6]}/>
             </tbody>
         </table>);
     }
